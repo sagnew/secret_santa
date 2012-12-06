7b3cd26e0f6ee3730f70fd4d6125bbb34c202e3e
Sam <sagnew@jmatthews.us>
2012-12-06 15:09:47 +0400

    So after many hours of derping around learning how to use Mongo(finally) and the basics of Flask, I got it working. I couldn't get it to work with the original HTML file(I'll investigate this later), but I made my own to test it(ignore that one), and it is inserting people into the DB.

--- app.py
+++ app.py
@@ -3,26 +3,20 @@ from flask import render_template
 from flask import request
 from functions import *
 
-insert_into_db("Assmuncher", "Doucher", "fuck@fuck")
-insert_into_db("As", "Doucher", "fuck@fu3ck")
-insert_into_db("Assuncher", "Doucher", "fuck@f4uck")
-
-pair_users()
-
 app = Flask(__name__)
-
-class View(flask.views.MethodView):
-	def get(self):
-		return flask.render_template("templates/main.html")
-
-	def post(self):
-		return "Fuck you!"
 
 @app.route('/')
 def main_page():
 	return render_template('main.html')
 
+@app.route('/', methods=['POST'])
+def post_data():
+	name = request.form["_name"]
+	about = request.form["_about"]
+	email = request.form["_email"]
+	insert_into_db(name, about, email)
+
 if __name__ == '__main__':
-	app.run(debug=False)
+	app.run(host='0.0.0.0')
 
 
--- functions.py
+++ functions.py
@@ -6,17 +6,28 @@ connection = pymongo.Connection()
 db = connection.secret_santa
 collection = db.gift_givers
 
+#This stores the people who tried to get multiple gifts so we can laugh at them later.
+naughty_list = db.naughty_list
+
+class MultipleUserException(Exception):
+#Exception thrown when a user tries to enter him/her self into the database twice
+#This means that the user is totally trying to gip USACS into giving them extra presents.
+	def __init__(self, message):
+		self.message = message
+	
 def insert_into_db(name, about, email):
 	#Inserts a user's information into the collection
-
-	#Only insert if the user is not already in the collection
-	if collection.find({"name": name, "email": email}).count() == 0:
-		post = {"name": name,
+	post = {"name": name,
 				"about": about,
 				"email": email,
 				"random number": random_number()
 				}
+	#Only insert if the user is not already in the collection
+	if collection.find({"name": name, "email": email}).count() == 0:
 		collection.insert(post)
+	else:
+		naughty_list.insert(post)
+		raise MultipleUserException("You have been added to the naughty list!") 
 
 def random_number():
 	#generates a random integer
@@ -25,14 +36,21 @@ def random_number():
 
 def pair_users():
 	#Pairs users randomly with each other
-
+	
 	users = []
+	paired = ""
 	#This is retarded, and my brain is not working
+	#Note to self out of laziness:
+	#Because the collection is sorted in a random order, I will take each person,
+	#and pair them up with the next person in the collection. The last person will 
+	#be paired with the first. This way, there will be no repeats. 
 	for item in collection.find().sort("random number", 1):
 		users.append(item)
-	print random.choice(users)
+		
 
 def send_email(recipient, name, about, email):
 	#Do this later with sendgrid
 	return
 
+for user in collection.find():
+	print user
