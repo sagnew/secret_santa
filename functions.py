b7f63423aa702518901ec7817c6a8b490a46b716
Samuel Agnew <sagnew@java.rutgers.edu>
2012-12-06 00:18:01 -0500

    Finally somewhat know how to use Mongo

--- functions.py
+++ functions.py
@@ -8,20 +8,15 @@ collection = db.gift_givers
 def insert_into_db(name, about, email):
 	#Inserts a user's information into the collection
 
-	if name not in collection.find("{name: {$in: [name] } }"):
-		collection.insert(post)
-	else:
+	#Only insert if the user is not already in the collection
+	if collection.find({"name": name, "email": email}).count() == 0:
 		post = {"name": name,
 				"about": about,
 				"email": email
 				}
+		collection.insert(post)
 
-collection.insert({"name": "ass",
-				"about": "ficl",
-				"email": "shit"
-				})
-collection.insert({"name": "hello",
-				"about": "ficl",
-				"email": "shit"
-				})
-insert_into_db("ass", "fuck", "shit")
+def send_email(recipient, name, about, email):
+	#Do this later with sendgrid
+	return
+
