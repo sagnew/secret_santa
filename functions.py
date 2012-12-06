f52a55bf63e8b269e2a52ba6f0fa563566edfe4c
Samuel Agnew <sagnew@java.rutgers.edu>
2012-12-05 21:46:30 -0500

    Need to check something

--- functions.py
+++ functions.py
@@ -1,1 +1,11 @@
 import pymongo
+
+def insert_into_db(name, about, email):
+	#Inserts a user's information into the collection
+	post = {"name": name,
+			"about": about,
+			"email": email
+			}
+	
+	if post not in collections:
+		collections.insert(post)
