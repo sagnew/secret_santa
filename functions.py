039bd7313335bb83db78b43cc6272e0f52fead97
Samuel Agnew <sagnew@java.rutgers.edu>
2012-12-05 22:13:03 -0500

    Ignore this

--- functions.py
+++ functions.py
@@ -1,11 +1,27 @@
 import pymongo
+import sendgrid
+
+connection = pymongo.Connection()
+db = connection.secret_santa
+collection = db.gift_givers
 
 def insert_into_db(name, about, email):
 	#Inserts a user's information into the collection
-	post = {"name": name,
-			"about": about,
-			"email": email
-			}
-	
-	if post not in collections:
-		collections.insert(post)
+
+	if name not in collection.find("{name: {$in: [name] } }"):
+		collection.insert(post)
+	else:
+		post = {"name": name,
+				"about": about,
+				"email": email
+				}
+
+collection.insert({"name": "ass",
+				"about": "ficl",
+				"email": "shit"
+				})
+collection.insert({"name": "hello",
+				"about": "ficl",
+				"email": "shit"
+				})
+insert_into_db("ass", "fuck", "shit")
