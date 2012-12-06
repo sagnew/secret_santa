22824ae65453402c90070eb47a7de5575b079009
Samuel Agnew <sagnew@java.rutgers.edu>
2012-12-06 02:27:59 -0500

    test

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import render_template
 from flask import request
 from functions import *
 
+insert_into_db("Assmuncher", "Doucher", "fuck@fuck")
+insert_into_db("As", "Doucher", "fuck@fu3ck")
+insert_into_db("Assuncher", "Doucher", "fuck@f4uck")
+
+pair_users()
+
 app = Flask(__name__)
 
 class View(flask.views.MethodView):
