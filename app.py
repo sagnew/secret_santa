ebc42e9cbeac4b20b1544345ad26f3c52c0d278f
Samuel Agnew <sagnew@java.rutgers.edu>
2012-12-06 02:18:39 -0500

    Testing something

--- app.py
+++ app.py
@@ -5,9 +5,18 @@ from functions import *
 
 app = Flask(__name__)
 
+class View(flask.views.MethodView):
+	def get(self):
+		return flask.render_template("templates/main.html")
+
+	def post(self):
+		return "Fuck you!"
+
 @app.route('/')
 def main_page():
 	return render_template('main.html')
 
 if __name__ == '__main__':
 	app.run(debug=False)
+
+
--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import pymongo
 import sendgrid
+import random
 
 connection = pymongo.Connection()
 db = connection.secret_santa
@@ -12,9 +13,24 @@ def insert_into_db(name, about, email):
 	if collection.find({"name": name, "email": email}).count() == 0:
 		post = {"name": name,
 				"about": about,
-				"email": email
+				"email": email,
+				"random number": random_number()
 				}
 		collection.insert(post)
+
+def random_number():
+	#generates a random integer
+	random.seed()
+	return random.randint(0, 99999)
+
+def pair_users():
+	#Pairs users randomly with each other
+
+	users = []
+	#This is retarded, and my brain is not working
+	for item in collection.find().sort("random number", 1):
+		users.append(item)
+	print random.choice(users)
 
 def send_email(recipient, name, about, email):
 	#Do this later with sendgrid
